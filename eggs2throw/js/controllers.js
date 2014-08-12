ca844d7932c27b0339a963dac606c6255b1f88a2
James <james@jamesedwardalexander.com>
2014-08-12 16:12:24 -0700

    initial front end with angular

---
+++ eggs2throw/js/controllers.js
@@ -0,0 +1,5 @@
+﻿var eggs2throw = angular.module('eggs2throw', []);
+
+eggs2throw.controller('movieView', ['$scope', function ($scope) {
+    $scope.movies = [{id : 0, title : 'Interloper'}, {id : 1, title : 'Starfox'}, {id : 2, title : 'Zelda Zombies'}];
+}]);
