d9a10346c4e6d0959ac36199447474469c4a490d
James <james@jamesedwardalexander.com>
2014-08-12 16:19:57 -0700

    unused folders deleted

--- eggs2throw/js/models.js
+++ eggs2throw/js/models.js
@@ -1,1 +1,10 @@
-﻿
+﻿app.factory('ajaxService', function ($resource) {
+
+    return {
+        getEvent: function () {
+            return $resource('/api/', {}, {
+                jData: { method: 'get', params: {}, isArray: true }
+            });
+        }
+    }
+});
