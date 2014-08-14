34359e6205ddb4f6da642f9da8f5cddfc0ceb35e
James <james@jamesedwardalexander.com>
2014-08-14 12:07:49 -0700

    fixed folder layout that caused git problem

---
+++ eggs2throw/js/controllers.js
@@ -0,0 +1,6 @@
+﻿var eggs2throw = angular.module('eggs2throw', ['ngResource']);
+
+eggs2throw.controller('movieView', ['$scope', 'ajaxService', function ($scope, ajaxService) {
+    $scope.movies = ajaxService.getEvent().jData();
+    console.log($scope.movies);
+}]);
